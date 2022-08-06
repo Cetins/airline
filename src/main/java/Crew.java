38a7c42077a6027748338ef7aacee60d93777de0
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-06 13:00:26 +0100

    crew abstract class and rank enum added

---
+++ src/main/java/Crew.java
@@ -0,0 +1,9 @@
+public abstract class Crew  {
+    private String name;
+    private Rank rank;
+
+    public Crew(String name, Rank rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+}
---
+++ src/main/java/Rank.java
@@ -0,0 +1,11 @@
+public enum Rank {
+    SECOND_OFFICER,
+    FIRST_OFFICER,
+    CAPTAIN,
+    HOSTESS,
+    STEWARD,
+    CABIN_CHIEF;
+
+    Rank() {
+    }
+}

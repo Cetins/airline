4912f104a29ceff84ecd5d61e6d0bae61e8e0e8e
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-06 12:20:09 +0100

    first commit

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    Passenger john;
+
+    @Before
+    public void before() {
+        john = new Passenger("John", 2);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("John", john.getName);
+    }
+}
